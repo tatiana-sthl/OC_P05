6bf805d19e79c5c11f2f0f2f832d472c41c4106c
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2023-01-15 17:20:30 +0100

    display a specific product ok

---
+++ front/js/product.js
@@ -0,0 +1,38 @@
+// Affiliate the product with its id
+
+const productId = new URLSearchParams(document.location.search).get("id");
+//console.log(productId);
+
+fetch(`http://localhost:3000/api/products/${productId}`)
+
+    .then((response) => response.json())
+
+    .then((selectedProduct) => {
+        // Display of api data
+        document.title = selectedProduct.name;
+
+        // Display image
+        let selectedProductImage = document.createElement("img");
+        document.getElementsByClassName("item__img")[0].appendChild(selectedProductImage);
+        selectedProductImage.src = selectedProduct.imageUrl;
+        selectedProductImage.alt = selectedProduct.altTxt;
+
+        // Dislay other elements
+        document.getElementById("title").innerText = selectedProduct.name;
+        document.getElementById("price").innerText = selectedProduct.price;
+        document.getElementById("description").innerText = selectedProduct.description;
+
+        // Display colors option
+        for (let i=0; i<selectedProduct.colors.length; i++) {
+            let productColors = document.createElement("option");
+            productColors.setAttribute("value", selectedProduct.colors[i]);
+            productColors.innerHTML = selectedProduct.colors[i];
+            document.getElementById("colors").appendChild(productColors);
+        }
+    })
+
+    .catch((error) => {
+        alert("Une erreur s'est produite, veuillez nous excuser");
+        console.log("fetch error in product.js", error);
+    })
+
